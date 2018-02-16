3caf5e4710e6c58a26dfa83423bc5a6fc29af452
Sebastian Coronado <sebastian@amigocloud.com>
2018-02-15 20:41:29 -0500

    + Added lines contours

--- main.cpp
+++ main.cpp
@@ -49,32 +49,85 @@ int main(int argc, const char * argv[])
     clock_t start, end;
     
     start = end = 0;
+    RNG rng(12345);
     
+    Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
     while (true)
     {
         start = clock();
-
+        
         if (!vid.read(frame))
             break;
         
         vector<Vec2f> foundpoints;
         
-        bool found = false;
+        //bool found = false;
         
-        threshold(frame, drawToFrame, 100, 255, THRESH_BINARY_INV);
+        threshold(frame, drawToFrame, 144, 255, THRESH_BINARY_INV);
         floodFill(drawToFrame, cv::Point(0,0), Scalar(255));
-        bitwise_not(drawToFrame, drawToFrame);
-        GaussianBlur( drawToFrame, drawToFrame, Size(9, 9), 2, 2 );
         cvtColor( drawToFrame, drawToFrame, COLOR_BGR2GRAY );
-        Canny( drawToFrame, drawToFrame, 0, 0,
-           3);
-        found = findCirclesGrid(drawToFrame, chessboardDimension, foundpoints,  CALIB_CB_SYMMETRIC_GRID);
-        drawChessboardCorners(drawToFrame, chessboardDimension, foundpoints, found);
+        bitwise_not(drawToFrame, drawToFrame);
+        //        GaussianBlur( drawToFrame, drawToFrame, Size(9, 9), 2, 2 );
+        
+        vector<vector<Point> > contours;
+        vector<Vec4i> hierarchy;
+        
+        findContours( drawToFrame, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
+        
+        vector<RotatedRect> minRect( contours.size() );
+        vector<RotatedRect> minEllipse( contours.size() );
+        vector<int> points;
+        
+        for( size_t i = 0; i < contours.size(); i++ )
+        {
+            minRect[i] = minAreaRect( Mat(contours[i]) );
+            if(contours[i].size() > 5 )
+            {
+                minEllipse[i] = fitEllipse( Mat(contours[i]) );
+            }
+        }
+        
+        int jj = 0;
+        for( size_t i = 0; i< contours.size(); i++ )
+        {
+            // contour
+            drawContours( drawToFrame, contours, (int)i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+            // ellipse
+            if (2500 > minEllipse[i].size.width * minEllipse[i].size.height && minEllipse[i].size.width * minEllipse[i].size.height > 100)
+            {
+                jj++;
+                points.push_back(i);
+                ellipse( drawToFrame, minEllipse[i], color, 2, 8 );
+                // rotated rectangle
+                Point2f rect_points[4];
+                minRect[i].points( rect_points );
+                for( int j = 0; j < 4; j++ )
+                    line( drawToFrame, rect_points[j], rect_points[(j+1)%4], color, 1, 8 );
+            }
+        }
+        
+        if(points.size() == 20)
+        {
+            for (int z = 1; z < points.size(); z++)
+            {
+                Point2f rect_points[4];
+                Point2f rect_points_1[4];
+                
+                
+                minRect[points[z-1]].points( rect_points );
+                minRect[points[z]].points( rect_points_1 );
+                //circle( drawToFrame, rect_points[0],5, color, 5);
+                line(drawToFrame, rect_points[0], rect_points_1[1], color, 5);
+            }
+        }
+        
+        //found = findCirclesGrid(drawToFrame, chessboardDimension, foundpoints,  CALIB_CB_SYMMETRIC_GRID);
+        //drawChessboardCorners(drawToFrame, chessboardDimension, foundpoints, found);
         
         //cout << foundpoints.size() <<endl;
         
         //frame.copyTo(drawToFrame);
-
+        
         imshow("Webcam", drawToFrame);
         
         char character = waitKey(1000/framesPerSecond);
@@ -86,11 +139,11 @@ int main(int argc, const char * argv[])
         fps  = num_frames / seconds;
         cout << "Estimated frames per second : " << fps << endl;
         start = end = 0;
-        presicion += foundpoints.size() / (20.0);
+        presicion += contours.size() / (20.0);
         presicionT = presicion / counter;
         counter++;
         cout << "Relative Presicion : " << presicion <<endl;
-        cout << "Total Presicion : " << presicionT <<endl;
+        cout << "Total Presicion : " << jj <<endl;
     }
     return 0;
 }
